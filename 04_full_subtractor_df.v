543928c37846ef8606f6157935faf085fcc0d7e2
Maedeh Mahmoudi <maedeemahmoudi5@gmail.com>
2025-08-27 16:05:48 +0330

    remove all files

--- 04_full_subtractor_df.v
+++
@@ -1,7 +0,0 @@
-
-module full_sub_df( a , b , borrowIn ,difference , borrowOut );
-
-    assign difference = a^b^borrowIn ;
-    assign borrowOut = ( ((~a)&b) | (~(a^b)&borrowIn) );
-
-endmodule
--- 09_decoder_4to16_with_2to4.v
+++
@@ -1,9 +0,0 @@
-
-module decoder_4to16(
-
-    input [3:0]a,
-    input enable,
-    output [15:0]d
-
-    );
-
